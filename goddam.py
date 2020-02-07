7e3faf2ab647ba0c59b5012b654f9c5e4199b31b
epicpigslav688 <60785111+epicpigslav688@users.noreply.github.com>
2020-02-07 15:08:03 +0000

    Update goddam.py

--- goddam.py
+++ goddam.py
@@ -1,8 +1,9 @@
+from time import sleep as sleep
 stage = 0
 score = 0
-def text(line):
+def text():
 	check = 0 
-	file = file.open("dialog.txt","r")
+	file = open("dialog.txt","r")
 	while check != 1
 		line = file.readline()
 		if line.str() == (stage.str()+score.str()):
@@ -10,4 +11,33 @@ def text(line):
 	check = 0
 	while check != 1:
 		line = file.readline()
-		if line == decision
+		if line == "decision":
+			check = 1
+		elif: 
+			print(line)
+		sleep(0.7)
+	
+	file.close
+def decision():
+	global score
+	global stage
+	x = "q"
+	check = 0
+	file = open("decision.txt","r")
+	while check != 1:
+		line = file.readline()
+		if file.readline == (stage.str()+score.str()):
+			check = 1
+	des1 = file.readline()
+	des2 = file.readline()
+	while x != "a" or x != b":
+		x = input("a) this choice\nb) this choice\n")
+		x = x.lower()
+	if a == "a":
+		score += 1
+	elif:
+		score += -1
+	stage += 1
+def game():
+	text()
+	decision()
