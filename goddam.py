6274b41868ece28adb55d42efb6407a19f91dcc3
epicpigslav688 <60785111+epicpigslav688@users.noreply.github.com>
2020-02-07 14:36:01 +0000

    Create goddam.py

---
+++ goddam.py
@@ -0,0 +1,13 @@
+stage = 0
+score = 0
+def text(line):
+	check = 0 
+	file = file.open("dialog.txt","r")
+	while check != 1
+		line = file.readline()
+		if line.str() == (stage.str()+score.str()):
+		check = 1
+	check = 0
+	while check != 1:
+		line = file.readline()
+		if line == decision
